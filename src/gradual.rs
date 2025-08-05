69e215ed54c000cb671ee3429222253d9969b40e
victor7w7r <arkano036@gmail.com>
2025-08-05 16:30:16 -0500

    AddPadScrollingFirstAttempt

--- src/gradual.rs
+++ src/gradual.rs
@@ -1,91 +1,37 @@
-use std::cmp::min;
-use std::thread::sleep;
-
-use evdev::{EventType, InputEvent, RelativeAxisCode, SynchronizationCode, uinput::VirtualDevice};
-use std::io::Result;
-
-#[derive(PartialEq, Copy, Clone, Default, Debug)]
-pub struct GradualMove {
-  pub x_direction: i32,
-  pub y_direction: i32,
-  pub both_move: i32,
-  pub move_only_x: i32,
-  pub move_only_y: i32,
+pub struct GradualScroll {
+  velocity: f64,
+  decay_factor: f64,
+  sensitivity: f64,
+  active_threshold: f64,
 }
 
-impl GradualMove {
-  pub fn calculate(x: i32, y: i32) -> Self {
-    let x_direction = x.signum();
-    let y_direction = y.signum();
-
-    let move_x = x.abs();
-    let move_y = y.abs();
-
-    let both_move = min(move_x, move_y);
-
-    let move_only_x = move_x - both_move;
-    let move_only_y = move_y - both_move;
-
+impl GradualScroll {
+  pub fn new(decay_factor: f64, sensitivity: f64) -> Self {
     Self {
-      x_direction,
-      y_direction,
-      both_move,
-      move_only_x,
-      move_only_y,
+      velocity: 0.0,
+      decay_factor,
+      sensitivity,
+      active_threshold: 0.1,
     }
   }
-}
 
-fn scroll_both(x: i32, y: i32, dev: &mut VirtualDevice) -> Result<()> {
-  dev.emit(&[InputEvent::new(
-    EventType::RELATIVE.0,
-    RelativeAxisCode::REL_WHEEL.0,
-    y,
-  )])?;
-  dev.emit(&[InputEvent::new(
-    EventType::RELATIVE.0,
-    RelativeAxisCode::REL_HWHEEL.0,
-    x,
-  )])?;
-  Ok(())
-}
-
-fn scroll_x(x: i32, dev: &mut VirtualDevice) -> Result<()> {
-  dev.emit(&[InputEvent::new(
-    EventType::RELATIVE.0,
-    RelativeAxisCode::REL_HWHEEL.0,
-    x,
-  )])?;
-  Ok(())
-}
-
-fn scroll_y(y: i32, dev: &mut VirtualDevice) -> Result<()> {
-  dev.emit(&[InputEvent::new(
-    EventType::RELATIVE.0,
-    RelativeAxisCode::REL_WHEEL.0,
-    y,
-  )])?;
-  Ok(())
-}
-
-pub fn smooth_scroll(x: i32, y: i32, dev: &mut VirtualDevice) -> Result<()> {
-  let gradual_move = GradualMove::calculate(x, y);
-
-  for _ in 0..gradual_move.both_move {
-    scroll_both(gradual_move.x_direction, gradual_move.y_direction, dev)?;
-  }
-  for _ in 0..gradual_move.move_only_x {
-    scroll_x(gradual_move.x_direction, dev)?;
-  }
-  for _ in 0..gradual_move.move_only_y {
-    scroll_y(gradual_move.y_direction, dev)?;
+  pub fn add_impulse(&mut self, impulse: f64) {
+    self.velocity += impulse * self.sensitivity
   }
 
-  dev.emit(&[InputEvent::new(
-    EventType::SYNCHRONIZATION.0,
-    SynchronizationCode::SYN_REPORT.0,
-    0,
-  )])?;
+  pub fn get_next_value(&mut self) -> f64 {
+    let current_value = self.velocity;
 
-  Ok(())
+    self.velocity *= self.decay_factor;
+
+    if self.velocity.abs() < self.active_threshold {
+      self.velocity = 0.0
+    }
+
+    current_value
+  }
+
+  pub fn is_active(&self) -> bool {
+    self.velocity.abs() >= self.active_threshold
+  }
 }
